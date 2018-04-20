ff7514217068c726edd5e812b75cce72f7e66a2e
david-binda <david.binovec@gmail.com>
2018-04-20 13:27:43 +0200

    3.0.2 release
    Bounces Stable tag (3.0.2), tested up to (4.9.5), and adds 3.0.2 Changelog:
    
    * Fixed issue with stale local cache in case remote ADD fails
    * Added wp_cache_get_multi function
    * Introduced max expiration value (30 days)
    * Added custom stats callback
    * Added tracking for SET operations
    * Fixed PHP notices for undefined keys in stats array
    * Code style improvements (Whitespacing, Yoda conditions ...)

--- object-cache.php
+++ object-cache.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Memcached
 Description: Memcached backend for the WP Object Cache.
-Version: 3.0.1
+Version: 3.0.2
 Plugin URI: http://wordpress.org/extend/plugins/memcached/
 Author: Ryan Boren, Denis de Bernardy, Matt Martz, Andy Skelton
 
